bfa956d5777ce6599978c10f01c3f38c2e805a5c
Yegor <45043399+rozd-yehor@users.noreply.github.com>
2021-05-31 13:08:01 +0300

    changed locators in test_task8.py

--- test_task8.py
+++ test_task8.py
@@ -17,9 +17,9 @@ def driver():
 
 def test_check_stickers(driver):
     driver.get("http://localhost/litecart/")
-    number_of_ducks = len(driver.find_elements_by_xpath("//a[contains(@title, 'Duck') and @class='link']"))
+    number_of_ducks = len(driver.find_elements_by_xpath("//li[contains(@class, 'product')]"))
     for n in range(0, number_of_ducks):
-        duck = driver.find_elements_by_xpath("//li[@class='product column shadow hover-light']")[n]
-        number_of_duck_stickers = len(duck.find_elements_by_xpath(".//div[starts-with(@class, 'sticker')]"))
+        duck = driver.find_elements_by_xpath("//li[contains(@class, 'product')]")[n]
+        number_of_duck_stickers = len(duck.find_elements_by_xpath(".//div[contains(@class, 'sticker')]"))
         if number_of_duck_stickers != 1:
             raise AssertionError("Number of duck's stickers is not equal to 1")
